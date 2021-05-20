e1ee0d0eae4ff6c6f7a17acc3a3b447667b145c0
Arkadiusz Adamik <arekadamik@gmail.com>
2021-05-20 10:19:50 +0200

    Complete function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -38,8 +38,51 @@ const titleClickHandler = function(event){
     targetArticle.classList.add('active');
   }
   
+ 
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  console.log(titleList);
+
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+ 
+  let html = '';
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+    console.log(articleId);
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+
+    /* get the title from the title element */
+    
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+  console.log(html);
+
   const links = document.querySelectorAll('.titles a');
+  console.log(links);
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
+
+generateTitleLinks();
